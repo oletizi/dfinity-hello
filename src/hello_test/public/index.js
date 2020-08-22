0211c40f79336feaa456bd5296cf26c3bdbc14bf
Orion Letizi <oletizi@mac.com>
2020-08-22 00:50:18 +0000

    more test infra

--- src/hello_test/public/index.js
+++ src/hello_test/public/index.js
@@ -1,1 +1,10 @@
+import describe from 'mocha';
+import it from 'mocha';
+
+describe('A nice test.', function() {
+    it('does stuff', function () {
+        window.alert('Doing stuff!')
+    })
+})
+mocha.run();
 window.alert('Hi!');
